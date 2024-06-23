9fad6bc1613f2ed793cc45115f2c2b35aa96247c
benckw69 <benckw69@gmail.com>
2024-06-23 15:09:29 +0800

    simpify moneytickets's logic

--- routes/moneyTickets.js
+++ routes/moneyTickets.js
@@ -6,16 +6,16 @@ const config = require('./config');
 const client = new MongoClient(config.url);
 
 /*  */
-router.get('/', (req, res) => {
-  if(req.session.user) {
-    res.render('moneyTickets_insert', { user:req.session.user, title:config.title });
-  }
+router.get('/insert', (req, res) => {
+  if(req.session.user) res.render('moneyTickets_insert', { user:req.session.user, title:config.title, msg:"" });
   else res.redirect('/');
 
 }).post('/', (req, res) => {
   if(req.session.user) {
     //need edit
-    res.render('moneyTickets_insert', { user:req.session.user, title:config.title });
+    let added = true;
+    if(added) res.render('moneyTickets_insert', { user:req.session.user, title:config.title, msg:"成功兌換現金卷" });
+    else res.render('moneyTickets_insert', { user:req.session.user, title:config.title, msg:"兌換現金卷失敗，請再嘗試" });
     let changeTicket = true;
 
   //connect to the database to see if ticket exist.  If exist, delete the ticket and add the money to the user
@@ -23,20 +23,21 @@ router.get('/', (req, res) => {
   if(changeTicket) res.redirect('/moneyTickets');
   }
   else res.redirect('/');
-}).get('/insert',(req,res)=>{
-  if(req.session.user) res.render('moneyTickets_insert', { user:req.session.user, title:config.title });
-  else res.redirect('/');
 }).get('/view',(req,res)=>{
   if(req.session.user) res.render('moneyTickets_view', { user:req.session.user, moneyTickets:[{code:"abc123", money:12}], title:config.title });
   else res.redirect('/');
 }).get('/new',(req,res)=>{
-  if(req.session.user) res.render('moneyTickets_new', { user:req.session.user, title:config.title });
+  if(req.session.user) res.render('moneyTickets_new', { user:req.session.user, title:config.title, msg:"" });
   else res.redirect('/');
 }).post('/new',(req,res)=>{
   if(req.session.user) {
     //need edit
-    let insert = true;
-    if(insert) res.redirect('/moneyTickets/new&error=false');
+    if(req.session.user) {
+      let insert = true;
+      if(insert) res.render('moneyTickets_new', { user:req.session.user, title:config.title, msg:"添加使用卷成功" });
+      else res.render('moneyTickets_new', { user:req.session.user, title:config.title, msg:"添加使用卷失敗，請再嘗試" });
+       
+    }
   }
   else res.redirect('/');
 });
