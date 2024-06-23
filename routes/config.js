98bec2662197a0d180e35e57211bcb68f3d59981
benckw69 <benckw69@gmail.com>
2024-06-23 11:11:00 +0800

    ignore a testing page.  Add header file and implement it at index.ejs.  Add config file and adding title variable.

--- routes/config.js
+++ routes/config.js
@@ -1,2 +1,20 @@
+const getTitle = async (url)=>{
+    const MongoClient = require('mongodb').MongoClient;
+    const client = new MongoClient(url);
+    try{
+        await client.connect();
+        const code_c = client.db("learningPlatform").collection("code");
+        let code = await code_c.findOne();
+        if(code) return code.title;
+        else return "error";
+    } finally {
+        client.close();
+    }
+}
+
 const url = "mongodb+srv://benckw69:Xp8HRCGYad@cluster0.xhaav3r.mongodb.net/";
-module.exports = { url }
+exports.url = url;
+const title_promise = getTitle(url);
+title_promise.then((result)=>{
+    exports.title = result;
+})
--- routes/index.js
+++ routes/index.js
@@ -9,21 +9,8 @@ const menuId = new ObjectId("6674d5e69626bba98ce76506");
 
 /* GET home page. */
 router.get('/', async (req, res)=> {
-  try {
-    await client.connect();
-    const users = client.db("learningPlatform").collection("code");
-    let menuCode = await users.findOne({_id:menuId});
-    if(menuCode){
-      if(req.session.user){
-        if(req.session.user.type== "student") res.render('index',{user:req.session.user, menuBar:menuCode.student_menuBar, title:menuCode.title});
-        if(req.session.user.type== "teacher") res.render('index',{user:req.session.user, menuBar:menuCode.teacher_menuBar, title:menuCode.title});
-        if(req.session.user.type== "admin") res.render('index',{user:req.session.user, menuBar:menuCode.admin_menuBar, title:menuCode.title});
-      }
-      else res.render('index',{user:{type:"",username:""},menuBar:"", title:menuCode.title});
-    } else res.send("網站錯誤");
-  } finally {
-      await client.close();
-  }
+  res.render('index',{user:req.session.user, title:config.title});
+
 }).get('/logout', (req, res) => {
   //handle logout and redirect to '/'.
   delete req.session.user;
