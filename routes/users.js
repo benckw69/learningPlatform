5c3790d34fbeb650b047c5eb05455da32eba5678
benckw69 <166276425+benckw69@users.noreply.github.com>
2024-06-24 15:44:37 +0800

    added changing user information. Fix a minor bug

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,8 @@ const MongoClient = require('mongodb').MongoClient;
 const config = require('./config');
 const client = new MongoClient(config.url);
 const ObjectId = require('mongodb').ObjectId;
+const validator = require('email-validator');
+const bcrypt = require('bcrypt');
 
 /* GET users listing. */
 router.get('/', async (req, res) => {
@@ -33,6 +35,12 @@ router.get('/', async (req, res) => {
   let msgCode = req.query.msg, msg;
   if(msgCode=="1") msg= "更改資料成功";
   else if(msgCode=="2") msg = "更改資料失敗：密碼錯誤";
+  else if(msgCode=="3") msg = "更改資料失敗，請稍後再試";
+  else if(msgCode=="4") msg = "出現未知錯誤，請登出以確保沒有問題";
+  else if(msgCode=="5") msg = "電郵地址格式錯誤";
+  else if(msgCode=="6") msg = "用戶名稱不能為空";
+  else if(msgCode=="7") msg = "用戶介紹不能為空";
+  else if(msgCode=="8") msg = "錯誤：電郵地址已經存在，請更改其他電郵地址";
   if(req.session.user) {
 
     //edit user info api.  Take user data from database.  Handle error.
@@ -51,17 +59,45 @@ router.get('/', async (req, res) => {
     }
   }
   else res.redirect('/');
-}).post('/edit/personalInfo', (req, res) => {
-  if(req.session.user) {
-  
-    //update data from database.  need edit
-    let data = {type:type, email:req.body.email, username:req.body.username, id:id, money:123}
-
-    let canUpdate = true;
-    if(canUpdate ) res.redirect('/users/edit/personalInfo?msg=1');
-    else res.redirect('/users/edit/personalInfo?msg=2');
-  }
-  res.redirect('/');
+}).post('/edit/personalInfo', async(req, res) => {
+  if(req.session.user && req.body) {
+    let user_new = structuredClone(req.session.user);
+    
+    user_new.email = req.body.email;
+    user_new.username = req.body.username;
+    if(req.session.user.type=="teacher"){
+      user_new.introduction = req.body.introduction;
+    }
+    if (!validator.validate(user_new.email)) res.redirect('/users/edit/personalInfo?msg=5');
+    else if (user_new.length == 0) res.redirect('/users/edit/personalInfo?msg=6');
+    else if (req.session.user.type=="teacher" && user_new.introduction.length == 0) res.redirect('/users/edit/personalInfo?msg=7');
+    else if(!bcrypt.compareSync(req.body.password, req.session.user.password)) res.redirect('/users/edit/personalInfo?msg=2');
+    else {
+      try{
+        await client.connect();
+        const users_c = client.db("learningPlatform").collection("users");
+        const email_repeat = await users_c.findOne({email:req.body.email, type:req.session.user.type});
+        if(email_repeat && email_repeat._id.toString() != req.session.user._id) res.redirect('/users/edit/personalInfo?msg=8');
+        else{
+          delete user_new._id;
+          const result = await users_c.replaceOne({_id:new ObjectId(req.session.user._id)}, user_new);
+          if(result.acknowledged) {
+            const result2 = await users_c.findOne({_id:new ObjectId(req.session.user._id)});
+            if(result2){
+              req.session.user = result2;
+              res.redirect('/users/edit/personalInfo?msg=1');
+            }
+            else res.redirect('/users/edit/personalInfo?msg=4');
+          }
+          else res.redirect('/users/edit/personalInfo?msg=3');
+        }
+        
+      } finally{
+          await client.close();
+      }
+    }
+    
+  } else res.redirect('/');
     
 }).get('/edit/password',(req,res)=>{
   let msgCode = req.query.msg, msg;
