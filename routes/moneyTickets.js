0dfb88c76758f1493eb509bb2eabd69dd562f4a1
benckw69 <166276425+benckw69@users.noreply.github.com>
2024-06-25 12:30:32 +0800

    add insert new tickets function

--- routes/moneyTickets.js
+++ routes/moneyTickets.js
@@ -14,9 +14,10 @@ router.get('/insert', (req, res) => {
   if(req.session.user&& req.session.user.type=="student") res.render('moneyTickets_insert', { user:req.session.user, title:config.title, msg:msg });
   else res.redirect('/');
 
-}).post('/insert', (req, res) => {
+}).post('/insert', async(req, res) => {
   if(req.session.user && req.session.user.type=="student") {
     //need edit
+    
     let added = true;
     if(added) res.redirect('/moneyTickets/insert?msg=1');
     else res.redirect('/moneyTickets/insert?msg=2');
@@ -28,21 +29,49 @@ router.get('/insert', (req, res) => {
   }
   else res.redirect('/');
 }).get('/view',(req,res)=>{
-  if(req.session.user && req.session.user.type=="admin") res.render('moneyTickets_view', { user:req.session.user, moneyTickets:[{code:"abc123", money:12}], title:config.title });
+  let msgCode=req.query.msg, msg;
+  if(msgCode=="1") msg= "刪除成功";
+  else if(msgCode=="2") msg = "刪除失敗，請稍後再試";
+
+  if(req.session.user && req.session.user.type=="admin") res.render('moneyTickets_view', { user:req.session.user, moneyTickets:[{code:"abc123", money:12}], title:config.title, pop:msg });
   else res.redirect('/');
 }).get('/new',(req,res)=>{
   let msgCode=req.query.msg, msg;
   if(msgCode=="1") msg= "添加使用卷成功";
-  else if(msgCode=="2") msg = "添加使用卷失敗，請再嘗試";
+  else if(msgCode=="2") msg = "已有此使用卷號碼，請嘗試輸入其他號碼";
+  else if(msgCode=="3") msg = "號碼不能為空，請輸入號碼";
+  else if(msgCode=="4") msg = "未能插入新紀錄，請稍後再試";
 
   if(req.session.user && req.session.user.type=="admin") res.render('moneyTickets_new', { user:req.session.user, title:config.title, msg:msg });
   else res.redirect('/');
-}).post('/new',(req,res)=>{
+}).post('/new', async(req,res)=>{
+  if(req.session.user && req.session.user.type=="admin") {
+    let code = req.body.code, money = req.body.money;
+    const moneyTickets_new = {code:code, money:money};
+    if(code.length==0) res.redirect('/moneyTickets/new?msg=3');
+    else {
+      try {
+        await client.connect();
+        const moneyTickets_c = client.db("learningPlatform").collection("moneyTickets");  
+        const moneyTicketsExist = await moneyTickets_c.findOne({code:code});
+        if(moneyTicketsExist) res.redirect('/moneyTickets/new?msg=2');
+        else {
+          const moneyTickets_insert = await moneyTickets_c.insertOne(moneyTickets_new);
+          if(moneyTickets_insert.acknowledged) res.redirect('/moneyTickets/new?msg=1');
+          else res.redirect('/moneyTickets/new?msg=4');
+        }
+      } finally {
+        await client.close();
+      }
+    }
+  }
+  else res.redirect('/');
+}).get('/delete',(req,res)=>{
   if(req.session.user && req.session.user.type=="admin") {
     //need edit
     let insert = true;
-    if(insert) res.redirect('/moneyTickets/new?msg=1');
-    else res.redirect('/moneyTickets/new?msg=2');
+    if(insert) res.redirect('/moneyTickets/view?msg=1');
+    else res.redirect('/moneyTickets/view?msg=2');
       
     
   }
