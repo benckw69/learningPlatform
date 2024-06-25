8bc79594d934ecef3b23e116580abd943f23b230
benckw69 <166276425+benckw69@users.noreply.github.com>
2024-06-25 17:02:03 +0800

    add moneyTickets adding money function

--- routes/moneyTickets.js
+++ routes/moneyTickets.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 const MongoClient = require('mongodb').MongoClient;
 const config = require('./config');
+const { ObjectId } = require('mongodb');
 const client = new MongoClient(config.url);
 
 /*  */
@@ -10,6 +11,10 @@ router.get('/insert', (req, res) => {
   let msgCode = req.query.msg, msg;
   if(msgCode=="1") msg= "成功兌換現金卷";
   else if(msgCode=="2") msg = "兌換現金卷失敗，請再嘗試";
+  else if(msgCode=="3") msg = "兌換現金卷失敗：號碼錯誤，請更改號碼後再嘗試";
+  else if(msgCode=="4") msg = "兌換現金卷失敗：伺服器無法刪除紀錄，請稍後再嘗試";
+  else if(msgCode=="5") msg = "兌換現金卷失敗：伺服器無法找到用家，請稍後再嘗試";
+  else if(msgCode=="6") msg = "兌換現金卷失敗：伺服器無法新增現金給予用家，請稍後再嘗試";
 
   if(req.session.user&& req.session.user.type=="student") res.render('moneyTickets_insert', { user:req.session.user, title:config.title, msg:msg });
   else res.redirect('/');
@@ -18,22 +23,48 @@ router.get('/insert', (req, res) => {
   if(req.session.user && req.session.user.type=="student") {
     //need edit
     
-    let added = true;
-    if(added) res.redirect('/moneyTickets/insert?msg=1');
-    else res.redirect('/moneyTickets/insert?msg=2');
-    let changeTicket = true;
-
-  //connect to the database to see if ticket exist.  If exist, delete the ticket and add the money to the user
-  
-  if(changeTicket) res.redirect('/moneyTickets');
+    let code=req.body.ticketNum;
+    let user=req.session.user;
+    try {
+      await client.connect();
+      const moneyTickets_c = client.db("learningPlatform").collection("moneyTickets");
+      const getMoneyTickets = await moneyTickets_c.findOne({code:code});
+      if(getMoneyTickets) {
+        //delete the tickets and modify the value of money of user
+        const deleteMoneyTickets = await moneyTickets_c.deleteOne({code:code});
+        if(deleteMoneyTickets.deletedCount == 1) {
+          const users_c = client.db("learningPlatform").collection("users");
+          const getUser = await users_c.findOne({_id:new ObjectId(user._id)});
+          if(getUser) {
+            getUser.money =Number(getUser.money) + Number(getMoneyTickets.money);
+            delete getUser._id;
+            const addMoneyToUser = await users_c.replaceOne({_id:new ObjectId(user._id)},getUser);
+            if(addMoneyToUser) res.redirect('/moneyTickets/insert?msg=1');
+            else res.redirect('/moneyTickets/insert?msg=6');
+          } else res.redirect('/moneyTickets/insert?msg=5');
+        } else res.redirect('/moneyTickets/insert?msg=4');
+      }
+      //when there is no code in database, return error message
+      else res.redirect('/moneyTickets/insert?msg=3');
+    } finally {
+      await client.close();
+    }
   }
   else res.redirect('/');
-}).get('/view',(req,res)=>{
+}).get('/view', async(req,res)=>{
   let msgCode=req.query.msg, msg;
   if(msgCode=="1") msg= "刪除成功";
   else if(msgCode=="2") msg = "刪除失敗，請稍後再試";
-
-  if(req.session.user && req.session.user.type=="admin") res.render('moneyTickets_view', { user:req.session.user, moneyTickets:[{code:"abc123", money:12}], title:config.title, pop:msg });
+  if(req.session.user && req.session.user.type=="admin") {
+    try{
+      await client.connect();
+      const moneyTickets_c = client.db("learningPlatform").collection("moneyTickets");  
+      const moneyTickets = await moneyTickets_c.find().toArray();
+      res.render('moneyTickets_view', { user:req.session.user, moneyTickets:moneyTickets, title:config.title, pop:msg });
+    } finally {
+      await client.close();
+    }
+  }
   else res.redirect('/');
 }).get('/new',(req,res)=>{
   let msgCode=req.query.msg, msg;
@@ -66,14 +97,20 @@ router.get('/insert', (req, res) => {
     }
   }
   else res.redirect('/');
-}).get('/delete',(req,res)=>{
+}).get('/delete',async(req,res)=>{
   if(req.session.user && req.session.user.type=="admin") {
-    //need edit
-    let insert = true;
-    if(insert) res.redirect('/moneyTickets/view?msg=1');
-    else res.redirect('/moneyTickets/view?msg=2');
-      
-    
+    if(req.query.id && req.query.id.length==24){
+      let id=req.query.id;
+      try {
+        await client.connect();
+        const moneyTickets_c = client.db("learningPlatform").collection("moneyTickets");
+        const deleteMoneyTickets = await moneyTickets_c.deleteOne({_id:new ObjectId(id)});
+        if(deleteMoneyTickets.deletedCount == 1) res.redirect("/moneyTickets/view?msg=1");
+        else res.redirect('/moneyTickets/view?msg=2');
+      } finally {
+        await client.close();
+      }
+    } else res.redirect('/moneyTickets/view');
   }
   else res.redirect('/');
 });
