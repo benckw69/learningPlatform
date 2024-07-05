d68594d608f4bf8f07bcf0983e674bb2c49da1ef
benckw69 <benckw69@gmail.com>
2024-07-05 23:05:46 +0800

    fix some routing and edit some method

--- routes/users.js
+++ routes/users.js
@@ -2,110 +2,75 @@ var express = require('express');
 var router = express.Router();
 
 const MongoClient = require('mongodb').MongoClient;
-const config = require('./config');
-const client = new MongoClient(config.url);
+const {url,db} = require('./config');
+const client = new MongoClient(url);
 const ObjectId = require('mongodb').ObjectId;
+const users_c = client.db(db).collection("users"); 
+const buyRecords = client.db(db).collection("buyRecords");
 const validator = require('email-validator');
 const bcrypt = require('bcrypt');
 const auth = require('./auth');
 
 /* GET users listing. */
 router.get('/', auth.islogin, async (req, res) => {
-
-    //view api.  Take user data from database
-    try {
-      await client.connect();
-      const users_c = client.db("learningPlatform").collection("users");  
-      let user = await users_c.findOne({_id:new ObjectId(req.user._id)});
-      if(user){
-        if(req.user.type == "student") res.render('users_view_student');
-        else if(req.user.type == "teacher") res.render('users_view_teacher');
-        else if(req.user.type == "admin") res.render('users_view_admin');
-      }
-      else {
-        res.redirect('/');
-      }
-    } finally {
-        await client.close();
-    }
+  if(req.user.type == "student") res.render('users_view_student');
+  else if(req.user.type == "teacher") res.render('users_view_teacher');
+  else if(req.user.type == "admin") res.render('users_view_admin');
 
 }).get('/edit/personalInfo', auth.islogin, async (req, res) => {
-  let msgCode = req.query.msg, msg;
-  if(msgCode=="1") msg= "更改資料成功";
-  else if(msgCode=="2") msg = "更改資料失敗：密碼錯誤";
-  else if(msgCode=="3") msg = "更改資料失敗，請稍後再試";
-  else if(msgCode=="4") msg = "出現未知錯誤，請登出以確保沒有問題";
-  else if(msgCode=="5") msg = "電郵地址格式錯誤";
-  else if(msgCode=="6") msg = "用戶名稱不能為空";
-  else if(msgCode=="7") msg = "用戶介紹不能為空";
-  else if(msgCode=="8") msg = "錯誤：電郵地址已經存在，請更改其他電郵地址";
-
-    //edit user info api.  Take user data from database.  Handle error.
-  try {
-    await client.connect();
-    const users_c = client.db("learningPlatform").collection("users");  
-    let user = await users_c.findOne({_id:new ObjectId(req.user._id)});
-    if(user){
-      if(req.user.type == "student") res.render('users_edit_student', {msg:msg});
-      else if(req.user.type == "teacher") res.render('users_edit_teacher', {msg:msg});
-      else if(req.user.type == "admin") res.render('users_edit_admin', {msg:msg});
-    }
-    else res.redirect('/');
-  } finally {
-      await client.close();
-  }
+  if(req.user.type == "student") res.render('users_edit_student');
+  else if(req.user.type == "teacher") res.render('users_edit_teacher');
+  else if(req.user.type == "admin") res.render('users_edit_admin');
+    
 }).post('/edit/personalInfo', auth.islogin, async(req, res) => {
   let user_new = structuredClone(req.user);
+  req.session.messages = [];
   
   user_new.email = req.body.email;
   user_new.username = req.body.username;
   if(req.user.type=="teacher"){
     user_new.introduction = req.body.introduction;
   }
-  if (!validator.validate(user_new.email)) res.redirect('/users/edit/personalInfo?msg=5');
-  else if (user_new.username.length == 0) res.redirect('/users/edit/personalInfo?msg=6');
-  else if (req.user.type=="teacher" && user_new.introduction.length == 0) res.redirect('/users/edit/personalInfo?msg=7');
-  else if(!req.user.loginMethod=="google" && !bcrypt.compareSync(req.body.password, req.user.password)) res.redirect('/users/edit/personalInfo?msg=2');
+  if (!validator.validate(user_new.email)) req.session.messages.push("電郵地址格式錯誤");
+  if (user_new.username.length == 0) req.session.messages.push("用戶名稱不能為空");
+  if (req.user.type=="teacher" && user_new.introduction.length == 0) req.session.messages.push("用戶介紹不能為空");
+  if(req.user.loginMethod=="email" && !bcrypt.compareSync(req.body.password, req.user.password)) req.session.messages.push("輸入的密碼與伺服器不符");
+  if(req.session.messages.length != 0) res.redirect('/users/edit/personalInfo');
   else {
     try{
       await client.connect();
-      const users_c = client.db("learningPlatform").collection("users");
-      const email_repeat = await users_c.findOne({email:req.body.email, type:req.user.type});
-      if(!req.user.loginMethod=="google" && email_repeat && email_repeat._id.toString() != req.user._id) res.redirect('/users/edit/personalInfo?msg=8');
+      if(req.user.loginMethod == "email") {
+        const email_repeat = await users_c.findOne({email:req.body.email, type:req.user.type, loginMethod:"email"});
+        if(email_repeat && email_repeat._id.toString() != req.user._id) req.session.messages.push("錯誤：電郵地址已經存在，請更改其他電郵地址");
+      }
       else{
         delete user_new._id;
         const result = await users_c.replaceOne({_id:new ObjectId(req.user._id)}, user_new);
         if(result.acknowledged) {
           const result2 = await users_c.findOne({_id:new ObjectId(req.user._id)});
           if(result2){
-            res.redirect('/users/edit/personalInfo?msg=1');
+            req.session.messages.push("更改資料成功");
           }
-          else res.redirect('/users/edit/personalInfo?msg=4');
+          else req.session.messages.push("出現未知錯誤，請登出以確保沒有問題");
         }
-        else res.redirect('/users/edit/personalInfo?msg=3');
+        else req.session.messages.push("更改資料失敗，請稍後再試");
       }
-      
+      res.redirect('/users/edit/personalInfo');
     } finally{
         await client.close();
     }
   }
     
-    
 }).get('/edit/password', auth.islogin, (req,res)=>{
-  let msgCode = req.query.msg, msg;
-  if(msgCode=="1") msg= "更改密碼成功";
-  else if(msgCode=="2") msg = "原始密碼錯誤，請重新輸入";
-  else if(msgCode=="3") msg = "兩個輸入密碼並不一致，請重新輸入";
-  else if(msgCode=="4") msg = "密碼長度未夠8位，請重新輸入";
-  else if(msgCode=="5") msg = "更改資料失敗，請稍後再試";
-  else if(msgCode=="6") msg = "出現未知錯誤，請登出以確保沒有問題";
-  
-  res.render('users_edit_password',{msg:msg});
+  if(req.user.loginMethod=="email") res.render('users_edit_password');
+  else res.redirect('/users')
 
 }).post('/edit/password', auth.islogin, async (req,res)=>{
-  if(req.body.password_new != req.body.password_new2) res.redirect('/users/edit/password?msg=3');
-  else if(req.body.password_new < 8)  res.redirect('/users/edit/password?msg=4');
-  else if(!bcrypt.compareSync(req.body.password_o, req.user.password)) res.redirect('/users/edit/password?msg=2');
+  req.session.messages = [];
+  if(req.body.password_new != req.body.password_new2) req.session.messages.push("兩個輸入密碼並不一致，請重新輸入");
+  if(req.body.password_new < 8)  req.session.messages.push("密碼長度未夠8位，請重新輸入");
+  if(!bcrypt.compareSync(req.body.password_o, req.user.password)) req.session.messages.push("原始密碼錯誤，請重新輸入");
+  if(req.session.messages) res.redirect('/users/edit/password');
   else {
     const saltRounds = 10;
     const salt = bcrypt.genSaltSync(saltRounds);
@@ -115,16 +80,15 @@ router.get('/', auth.islogin, async (req, res) => {
     delete user_new._id;
     try{
       await client.connect();
-      const users_c = client.db("learningPlatform").collection("users");
       const result = await users_c.replaceOne({_id:new ObjectId(req.user._id)},user_new);
       if(result.acknowledged) {
         const result2 = await users_c.findOne({_id:new ObjectId(req.user._id)});
         if(result2){
-          res.redirect('/users/edit/password?msg=1');
+          req.session.messages.push("更改密碼成功");
         }
-        else res.redirect('/users/edit/password?msg=6');
+        else req.session.messages.push("出現未知錯誤，請登出以確保沒有問題");
       }
-      else res.redirect('/users/edit/password?msg=5');
+      else req.session.messages.push("更改資料失敗，請稍後再試");
     } finally {
       await client.close();
     }
@@ -134,11 +98,9 @@ router.get('/', auth.islogin, async (req, res) => {
   //delete user data and user paid course data.  Only allow teacher and student type to delete the account.  Need edit.
   let user = req.user;
   try {
-    await client.connect();
-    const users_c = client.db("learningPlatform").collection("users");  
+    await client.connect(); 
     let user_d = await users_c.deleteOne({_id:new ObjectId(user._id)});
     if(user_d){
-      const buyRecords = client.db("learningPlatform").collection("buyRecords");
       let user_d2 = await buyRecords.deleteMany({userId:new ObjectId(user._id)});
       req.logOut((err)=>{
         res.redirect('/?msg=1');
