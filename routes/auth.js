9ddebb0ece2776cdafb2720484f7bb7cde0028fc
benckw69 <166276425+benckw69@users.noreply.github.com>
2024-07-24 18:03:53 +0800

    change login redirect

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 exports.islogin = (req,res,next)=>{
     if(req.user) next();
-    else res.redirect('/login?type=student');
+    else res.redirect('/login');
 }
 exports.isNotlogin = (req,res,next)=>{
     if(!req.user) next();
@@ -9,20 +9,20 @@ exports.isNotlogin = (req,res,next)=>{
 exports.isloginByStudent = (req,res,next)=>{
     if(req.user && req.user.type == "student") next();
     else if(req.user) res.redirect('/');
-    else res.redirect('/login?type=student');
+    else res.redirect('/login');
 }
 exports.isloginByTeacher = (req,res,next)=>{
     if(req.user && req.user.type == "teacher") next();
     else if(req.user) res.redirect('/');
-    else res.redirect('/login?type=teacher');
+    else res.redirect('/login');
 }
 exports.isloginByStudentAndTeacher = (req,res,next)=>{
     if(req.user && (req.user.type == "teacher" || req.user.type == "student")) next();
     else if(req.user) res.redirect('/');
-    else res.redirect('/login?type=student');
+    else res.redirect('/login');
 }
 exports.isloginByAdmin = (req,res,next)=>{
     if(req.user && req.user.type == "admin") next();
     else if(req.user) res.redirect('/');
-    else res.redirect('/login?type=admin');
+    else res.redirect('/login');
 }
